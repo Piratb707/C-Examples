a94fcee6bb153395bf01882c73a5e07998a0c42d
PiraTb707 <beloshapkobm@yandex.ru>
2022-12-14 20:37:48 +0600

    add new stuff

--- Example005_Countifelse/Program.cs
+++ Example005_Countifelse/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.WriteLine("Введите Имя пользователя: ");
 string username = Console.ReadLine();
 
-if(username == "Маша")
+if(username.ToLower() == "Маша")
 {
     Console.WriteLine("Ура, это же МАША");
 }
--- Example006_Base/Program.cs
+++ Example006_Base/Program.cs
@@ -3,7 +3,6 @@ int b = 2;
 int c = 6;
 int d = 8;
 int e = 4;
-
 int max = a;
 
 if(a > max) max = a;
